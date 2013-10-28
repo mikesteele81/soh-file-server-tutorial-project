013bc8bafd2000a669a1cbb940e32f9cd53c2d0d
Michael Steele <mikesteele81@gmail.com>
2013-10-28 07:09:01 -0700

    part 5, example 6

---
+++ src/Model.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Model where
+
+import Data.ByteString (ByteString)
+import Data.Text (Text)
+import Database.Persist.Quasi
+import Yesod
+import Data.Typeable (Typeable)
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"]
+    $(persistFileWith lowerCaseSettings "config/models")
